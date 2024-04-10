2373a72c38d914e7398501b57d5e75871e082ecd
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-04-10 17:56:43 +0700

    update: Add routes and server setup in main.go

--- main.go
+++ main.go
@@ -1,1 +1,45 @@
 package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/textures1245/BlogDuaaeeg-backend/routes"
+	"github.com/textures1245/BlogDuaaeeg-backend/utils"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello, World!",
+		})
+	})
+
+	r.Run(":8080")
+}
+
+func runServer() {
+	// setup
+	r := gin.Default()
+	lg := utils.NewConsoleLogger(utils.Level("TRACE"))
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+		lg.Db.Info("Defaulting to port %s", port)
+	}
+
+	// routes definition
+	rG := r.Group("/api/v1")
+	routes.InitRoute(rG)
+
+	utils.DbConnect()
+
+	lg.Db.Info("Listening on port %s", port)
+	r.Run(":" + port)
+
+}

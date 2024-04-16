b5a660602df436699f0f0581e98824cdb79f2468
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-04-16 17:54:21 +0700

    refactor: Refactor HTTPError to CError

--- error/entity/error.go
+++ error/entity/error.go
@@ -1,12 +1,10 @@
 package entity
 
-type HTTPError struct {
-	Status     string      `json:"status"`
-	StatusCode int         `json:"status_code"`
-	Message    string      `json:"message"`
-	Result     interface{} `json:"result"`
+type CError struct {
+	StatusCode int   `json:"status_code"`
+	Err        error `json:"error"`
 }
 
-func (h *HTTPError) Error() string {
-	return h.Message
+func (e *CError) Error() string {
+	return e.Err.Error()
 }

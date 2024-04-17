31c8934ef292e3eb0266ec7677f933c8c1c2ac95
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-04-17 13:14:16 +0700

    refactor: Refactor user profile update function

--- model/user/repository/user_res.go
+++ model/user/repository/user_res.go
@@ -2,7 +2,6 @@ package repository
 
 import (
 	"context"
-	"fmt"
 	"net/http"
 
 	"github.com/go-playground/validator/v10"
@@ -75,36 +74,46 @@ func (u *UserRepo) UpdateProfile(userUuid string, req *_userEntity.UserProfileDa
 	ctx := context.Background()
 
 	validate := validator.New(validator.WithRequiredStructEnabled())
-	err := validate.Struct(req)
-	if err != nil {
-		errors := err.(validator.ValidationErrors)
-		return nil, &_errEntity.CError{
-			StatusCode: http.StatusBadRequest,
-			Err:        errors,
+	if err := validate.Struct(req); err != nil {
+		if errors, ok := err.(validator.ValidationErrors); ok {
+			return nil, &_errEntity.CError{
+				StatusCode: http.StatusBadRequest,
+				Err:        errors,
+			}
 		}
 	}
 
-	fmt.Println(userUuid)
+	updateUsrProfile, err := u.createOrUpdateUserProfile(&ctx, userUuid, req)
+	if err != nil {
+		return nil, err
+	}
 
-	user, err := u.Db.UserProfile.CreateOne(
+	return updateUsrProfile, nil
+}
+
+func (u *UserRepo) createOrUpdateUserProfile(ctx *context.Context, userUuid string, req *_userEntity.UserProfileDataRequest) (*db.UserProfileModel, error) {
+	userProfile, err := u.Db.UserProfile.UpsertOne(
+		db.UserProfile.UserUUID.Equals(userUuid),
+	).Create(
 		db.UserProfile.FirstName.Set(req.FirstName),
 		db.UserProfile.LastName.Set(req.LastName),
 		db.UserProfile.Bio.Set(req.Bio),
 		db.UserProfile.ProfilePicture.Set(req.ProfilePicture),
-
 		db.UserProfile.User.Link(
 			db.User.UUID.Equals(userUuid),
 		),
-	).Exec(ctx)
+	).Update(
+		db.UserProfile.FirstName.Set(req.FirstName),
+		db.UserProfile.LastName.Set(req.LastName),
+		db.UserProfile.Bio.Set(req.Bio),
+		db.UserProfile.ProfilePicture.Set(req.ProfilePicture),
+	).Exec(*ctx)
 	if err != nil {
-		fmt.Print(fmt.Errorf("error: %v", err.Error()))
-
 		return nil, &_errEntity.CError{
 			StatusCode: http.StatusInternalServerError,
 			Err:        err,
 		}
 	}
 
-	return user, nil
-
+	return userProfile, nil
 }

3a09f4cb3c4d1b4da7d1ada510d788dfc98ccf4c
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-04-21 18:18:35 +0700

    feat-imp (post-model): Add PostReqDat and PostResDat structs, FetchPostOpts struct, PostRepository and PostService interfaces

--- model/post/entity/post.go
+++ model/post/entity/post.go
@@ -1,8 +1,40 @@
 package entity
 
+import "github.com/textures1245/BlogDuaaeeg-backend/db"
+
 type PostReqDat struct {
 	UserUuid  string `json:"user_uuid" db:"user_uuid" form:"user_uuid" binding:"required" validate:"required"`
 	Title     string `json:"title" db:"title" form:"title" binding:"required" validate:"required"`
 	Content   string `json:"content" db:"content" form:"content" binding:"required" validate:"required"`
 	Published bool   `json:"published" db:"published" form:"published" binding:"required" validate:"required"`
+	SrcType   string `json:"src_type" db:"src_type" form:"src_type" binding:"required" validate:"required"`
+}
+
+type PostResDat struct {
+	UUID      string `json:"uuid"`
+	UserUuid  string `json:"user_uuid"`
+	Title     string `json:"title"`
+	Source    string `json:"source"`
+	Published bool   `json:"published"`
+	SrcType   string `json:"src_type"`
+}
+
+type FetchPostOpts struct {
+	Page int
+}
+
+type PostRepository interface {
+	CreatePost(req *PostReqDat) (*db.PostModel, error)
+	FetchPostByUUID(uuid string) (*db.PostModel, error)
+	FetchPublisherPosts(opts *FetchPostOpts) ([]db.PublicationPostModel, error)
+	UpdatePostByUUID(uuid string, req *PostReqDat) (*db.PostModel, error)
+	UpdatePostToPublisher(userUuid string, postUuid string) error
+}
+
+type PostService interface {
+	OnCreateNewPost(req *PostReqDat) (*PostResDat, error)
+	OnFetchPostByUUID(uuid string) (*PostResDat, error)
+	OnFetchPublisherPosts(opts *FetchPostOpts) ([]*PostResDat, error)
+	OnUpdatePostByUUID(uuid string, req *PostReqDat) (*PostResDat, error)
+	OnSubmitPostToPublisher(userUuid string, postUuid string) error
 }

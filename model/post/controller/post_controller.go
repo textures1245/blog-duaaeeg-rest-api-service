1e72fd288d4d540fc53c1487da361d40cf044a2b
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-04-21 20:42:17 +0700

    feat-imp(post-controller): Add post controller with CreatePost, UpdatePost, GetPostByUUID, GetPostByUserUUID, and GetPublisherPosts endpoints

---
+++ model/post/controller/post_controller.go
@@ -0,0 +1,170 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	_errEntity "github.com/textures1245/BlogDuaaeeg-backend/error/entity"
+	"github.com/textures1245/BlogDuaaeeg-backend/error/handler"
+	"github.com/textures1245/BlogDuaaeeg-backend/model/post/entity"
+)
+
+type postCon struct {
+	PostUse entity.PostService
+}
+
+func NewPostController(authService entity.PostService) *postCon {
+	return &postCon{
+		PostUse: authService,
+	}
+
+}
+
+func (h *postCon) CreatePost(c *gin.Context) {
+	req := new(entity.PostReqDat)
+	if err := c.ShouldBind(req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status":      http.StatusText(http.StatusBadRequest),
+			"status_code": http.StatusBadRequest,
+			"message":     err.Error(),
+			"result":      nil,
+		})
+		return
+	}
+
+	res, err := h.PostUse.OnCreateNewPost(req)
+	if err != nil {
+		handlerE := handler.NewHandler(&handler.HandleUse{})
+		hE := handlerE.PrismaPostHandle(*err.(*_errEntity.CError))
+		c.JSON(hE.StatusCode, gin.H{
+			"status":      http.StatusText(hE.StatusCode),
+			"status_code": hE.StatusCode,
+			"message":     hE.Error(),
+			"result":      nil,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"status":      "OK",
+		"status_code": http.StatusOK,
+		"message":     "",
+		"result":      res,
+	})
+}
+
+func (h *postCon) UpdatePost(c *gin.Context) {
+	req := new(entity.PostReqDat)
+	pUuid := c.Param("uuid")
+
+	if err := c.ShouldBind(req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status":      http.StatusText(http.StatusBadRequest),
+			"status_code": http.StatusBadRequest,
+			"message":     err.Error(),
+			"result":      nil,
+		})
+		return
+	}
+
+	res, err := h.PostUse.OnUpdatePostByUUID(pUuid, req)
+	if err != nil {
+		handlerE := handler.NewHandler(&handler.HandleUse{})
+		hE := handlerE.PrismaPostHandle(*err.(*_errEntity.CError))
+		c.JSON(hE.StatusCode, gin.H{
+			"status":      http.StatusText(hE.StatusCode),
+			"status_code": hE.StatusCode,
+			"message":     hE.Error(),
+			"result":      nil,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"status":      "OK",
+		"status_code": http.StatusOK,
+		"message":     "",
+		"result":      res,
+	})
+}
+
+func (h *postCon) GetPostByUUID(c *gin.Context) {
+	pUuid := c.Param("uuid")
+
+	res, err := h.PostUse.OnFetchPostByUUID(pUuid)
+	if err != nil {
+		handlerE := handler.NewHandler(&handler.HandleUse{})
+		hE := handlerE.PrismaPostHandle(*err.(*_errEntity.CError))
+		c.JSON(hE.StatusCode, gin.H{
+			"status":      http.StatusText(hE.StatusCode),
+			"status_code": hE.StatusCode,
+			"message":     hE.Error(),
+			"result":      nil,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"status":      "OK",
+		"status_code": http.StatusOK,
+		"message":     "",
+		"result":      res,
+	})
+}
+
+func (h *postCon) GetPostByUserUUID(c *gin.Context) {
+	uUuid := c.Param("uuid")
+
+	res, err := h.PostUse.OnFetchOwnerPosts(uUuid)
+	if err != nil {
+		handlerE := handler.NewHandler(&handler.HandleUse{})
+		hE := handlerE.PrismaPostHandle(*err.(*_errEntity.CError))
+		c.JSON(hE.StatusCode, gin.H{
+			"status":      http.StatusText(hE.StatusCode),
+			"status_code": hE.StatusCode,
+			"message":     hE.Error(),
+			"result":      nil,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"status":      "OK",
+		"status_code": http.StatusOK,
+		"message":     "",
+		"result":      res,
+	})
+}
+
+func (h *postCon) GetPublisherPosts(c *gin.Context) {
+	req := new(entity.FetchPostOptReq)
+	if err := c.ShouldBind(req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status":      http.StatusText(http.StatusBadRequest),
+			"status_code": http.StatusBadRequest,
+			"message":     err.Error(),
+			"result":      nil,
+		})
+		return
+	}
+
+	res, err := h.PostUse.OnFetchPublisherPosts(req)
+	if err != nil {
+		handlerE := handler.NewHandler(&handler.HandleUse{})
+		hE := handlerE.PrismaPostHandle(*err.(*_errEntity.CError))
+		c.JSON(hE.StatusCode, gin.H{
+			"status":      http.StatusText(hE.StatusCode),
+			"status_code": hE.StatusCode,
+			"message":     hE.Error(),
+			"result":      nil,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"status":      "OK",
+		"status_code": http.StatusOK,
+		"message":     "",
+		"result":      res,
+	})
+}

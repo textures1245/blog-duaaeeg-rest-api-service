22d622accfbf8947d6b06bd5dbaa2d4e5c1ee228
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-04-24 15:59:33 +0700

    feat-imp(comm-model): Add Comment entity, repository, and implementation

---
+++ model/post/repository/comment_res.go
@@ -0,0 +1,60 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/textures1245/BlogDuaaeeg-backend/db"
+	"github.com/textures1245/BlogDuaaeeg-backend/model/post/entity"
+)
+
+type CommRepo struct {
+	db *db.PrismaClient
+}
+
+func NewCommRepo(db *db.PrismaClient) entity.CommentRepository {
+	return &CommRepo{db}
+}
+
+func (c *CommRepo) CreateComment(pUuid string, req *entity.CommentReqDat) (*db.CommentModel, error) {
+	ctx := context.Background()
+
+	comm, err := c.db.Comment.CreateOne(
+		db.Comment.Content.Set(req.Comment),
+		db.Comment.User.Link(
+			db.User.UUID.Equals(req.UserUuid),
+		),
+		db.Comment.Post.Link(
+			db.Post.UUID.Equals(pUuid),
+		),
+	).Exec(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return comm, nil
+}
+
+func (c *CommRepo) UpdateCommentByUUID(cUuid string, req *entity.CommentReqDat) (*db.CommentModel, error) {
+	ctx := context.Background()
+
+	comm, err := c.db.Comment.FindUnique(
+		db.Comment.UUID.Equals(cUuid),
+	).Update(
+		db.Comment.Content.Set(req.Comment),
+	).Exec(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return comm, nil
+}
+
+func (c *CommRepo) DeleteCommentByUUID(cUuid string) error {
+	ctx := context.Background()
+
+	_, err := c.db.Comment.FindUnique(
+		db.Comment.UUID.Equals(cUuid),
+	).Delete().Exec(ctx)
+	if err != nil {
+		return err
+	}
+	return nil
+}

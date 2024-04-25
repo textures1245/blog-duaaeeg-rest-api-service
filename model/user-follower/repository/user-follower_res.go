b47ab669ad4caf5d83d5e3bb1baef22496f9a026
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-04-25 14:02:06 +0700

    feat-imp(usrFollower-model): implemented core feats by Add CreateUserFollower and DeleteUserFollowerByUUID methods to UsrFollowerRepo

---
+++ model/user-follower/repository/user-follower_res.go
@@ -0,0 +1,55 @@
+package repository
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/textures1245/BlogDuaaeeg-backend/db"
+	_errEntity "github.com/textures1245/BlogDuaaeeg-backend/error/entity"
+	"github.com/textures1245/BlogDuaaeeg-backend/model/user-follower/entity"
+)
+
+type UsrFollowerRepo struct {
+	db *db.PrismaClient
+}
+
+func NewUsrFollowerRepo(db *db.PrismaClient) entity.UserFollowerRepository {
+	return &UsrFollowerRepo{db}
+}
+
+func (u *UsrFollowerRepo) CreateUserFollower(usrFollowerUuid string, req *entity.UserFollowerReqDat) (*db.UserFollowerModel, error) {
+
+	ctx := context.Background()
+
+	res, err := u.db.UserFollower.CreateOne(
+		db.UserFollower.Followee.Link(
+			db.User.UUID.Equals(req.UserFolloweeUuid),
+		),
+		db.UserFollower.Follower.Link(
+			db.User.UUID.Equals(usrFollowerUuid),
+		),
+	).Exec(ctx)
+
+	if err != nil {
+		return nil, &_errEntity.CError{
+			Err:        err,
+			StatusCode: http.StatusInternalServerError,
+		}
+	}
+
+	return res, nil
+}
+
+func (u *UsrFollowerRepo) DeleteUserFollowerByUUID(usrFollowerUuid string, req *entity.UserFollowerReqDat) error {
+	ctx := context.Background()
+
+	_, err := u.db.Prisma.ExecuteRaw(`DELETE FROM "UserFollower" WHERE "followerUuid" = $1 AND "followeeUuid" = $2`, usrFollowerUuid, req.UserFolloweeUuid).Exec(ctx)
+	if err != nil {
+		return &_errEntity.CError{
+			Err:        err,
+			StatusCode: http.StatusInternalServerError,
+		}
+	}
+
+	return nil
+}

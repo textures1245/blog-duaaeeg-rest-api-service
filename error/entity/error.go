41063e9fe60fe68f9510c977d6553976640b188d
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-04-25 15:19:36 +0700

    update(err): Add PrismaCustomHandle method to ErrorHandler interface

--- error/entity/error.go
+++ error/entity/error.go
@@ -12,4 +12,5 @@ func (e *CError) Error() string {
 type ErrorHandler interface {
 	PrismaAuthHandle(err CError) *CError
 	PrismaPostHandle(err CError) *CError
+	PrismaCustomHandle(nameM string, err CError) *CError
 }
--- error/handler/prisma-handler.go
+++ error/handler/prisma-handler.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"errors"
+	"fmt"
 
 	"github.com/textures1245/BlogDuaaeeg-backend/db"
 	"github.com/textures1245/BlogDuaaeeg-backend/error/entity"
@@ -34,6 +35,22 @@ func (h *HandleUse) PrismaPostHandle(err entity.CError) *entity.CError {
 
 	if errors.Is(err.Err, db.ErrNotFound) {
 		err.Err = errors.New("PostNotFound")
+		return &err
+	} else {
+		return &err
+	}
+}
+
+func (h *HandleUse) PrismaCustomHandle(nameM string, err entity.CError) *entity.CError {
+
+	if errors.Is(err.Err, db.ErrNotFound) {
+		err.StatusCode = 404
+		err.Err = errors.New(nameM + "NotFound")
+		return &err
+	} else if info, errUnique := db.IsErrUniqueConstraint(err.Err); errUnique {
+		// Fields exists for Postgres and SQLite
+		err.StatusCode = 409
+		err.Err = fmt.Errorf("The action prevent for creating new unique constraint that already exist on the fields: %s", info.Fields)
 		return &err
 	} else {
 		return &err

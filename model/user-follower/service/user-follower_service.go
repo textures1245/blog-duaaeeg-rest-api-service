7a0d1d41f31873d025c6abf6987700100aa81ffd
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-04-25 14:02:25 +0700

    feat-imp(usrFollower-service): Add core UserFollowerService  implementation

---
+++ model/user-follower/service/user-follower_service.go
@@ -0,0 +1,32 @@
+package service
+
+import (
+	"github.com/textures1245/BlogDuaaeeg-backend/db"
+	"github.com/textures1245/BlogDuaaeeg-backend/model/user-follower/entity"
+)
+
+type usrFollowerUse struct {
+	usrFollowerRepo entity.UserFollowerRepository
+}
+
+func NewUsrFollowerService(usrFollowerRepo entity.UserFollowerRepository) entity.UserFollowerService {
+	return &usrFollowerUse{
+		usrFollowerRepo,
+	}
+}
+
+func (u *usrFollowerUse) OnSubscribeUser(usrFollowerUuid string, req *entity.UserFollowerReqDat) (*db.UserFollowerModel, error) {
+	usrFollower, err := u.usrFollowerRepo.CreateUserFollower(usrFollowerUuid, req)
+	if err != nil {
+		return nil, err
+	}
+	return usrFollower, nil
+}
+
+func (u *usrFollowerUse) OnUnsubscribeUser(usrFollowerUuid string, req *entity.UserFollowerReqDat) error {
+	err := u.usrFollowerRepo.DeleteUserFollowerByUUID(usrFollowerUuid, req)
+	if err != nil {
+		return err
+	}
+	return nil
+}

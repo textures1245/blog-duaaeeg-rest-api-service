08c72707bee53fd87bc252307173e08646aefa80
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-04-29 15:34:19 +0700

    refactor(clean): move handling package and handler functions into /pkg/error

---
+++ pkg/error/entity/error.go
@@ -0,0 +1,16 @@
+package entity
+
+type CError struct {
+	StatusCode int   `json:"status_code"`
+	Err        error `json:"error"`
+}
+
+func (e *CError) Error() string {
+	return e.Err.Error()
+}
+
+type ErrorHandler interface {
+	PrismaAuthHandle(err CError) *CError
+	PrismaPostHandle(err CError) *CError
+	PrismaCustomHandle(nameM string, err CError) *CError
+}

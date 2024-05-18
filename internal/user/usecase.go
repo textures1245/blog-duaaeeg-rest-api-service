e49ac7e336d77e225e6da68a05d1ef868871091c
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-05-18 17:17:08 +0700

    update(user-model): Add GetUsers and DeleteUserByUuid methods

--- internal/user/usecase.go
+++ internal/user/usecase.go
@@ -8,4 +8,7 @@ import (
 type UserService interface {
 	OnFetchUserByUUID(userUuid string) (*entities.UserResDat, error)
 	OnUpdateUserProfile(userUuid string, req *dtos.UserProfileDataRequest) (*entities.UserProfileRes, error)
+	OnFetchUsers() ([]*entities.UserResDat, error)
+	OnFetchUsersWithPW() ([]*entities.UserWithPWResDat, error)
+	OnDeleteUser(userUuid string) error
 }

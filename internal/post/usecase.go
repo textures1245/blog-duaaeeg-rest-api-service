0c5a8c2671095cfd8db756f8a09b6a3d6d02e299
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-05-21 00:24:10 +0700

    feat(post-usecase): Update post usecase to include file upload functionality

--- internal/post/usecase.go
+++ internal/post/usecase.go
@@ -1,16 +1,17 @@
 package post
 
 import (
+	"github.com/gin-gonic/gin"
 	_cateEntities "github.com/textures1245/BlogDuaaeeg-backend/internal/category/entities"
 	"github.com/textures1245/BlogDuaaeeg-backend/internal/post/dtos"
 	"github.com/textures1245/BlogDuaaeeg-backend/internal/post/entities"
 )
 
 type PostService interface {
-	OnCreateNewPost(cateResDat *_cateEntities.PostCategoryResDat, tagResDat *_cateEntities.PostTagResDat, req *dtos.PostReqDat) (*entities.PostResDat, error)
+	OnCreateNewPost(c *gin.Context, cateResDat *_cateEntities.PostCategoryResDat, tagResDat *_cateEntities.PostTagResDat, req *dtos.PostReqDat) (*entities.PostResDat, error)
 	OnFetchPostByUUID(uuid string) (*entities.PostResDat, error)
 	OnFetchPublisherPosts(opts *dtos.FetchPostOptReq) ([]*entities.PostResDat, error)
-	OnUpdatePostAndTagByUUID(cateResDat *_cateEntities.PostCategoryResDat, uuid string, req *dtos.PostReqDat) (*entities.PostResDat, error)
+	OnUpdatePostAndTagByUUID(c *gin.Context, cateResDat *_cateEntities.PostCategoryResDat, uuid string, req *dtos.PostReqDat) (*entities.PostResDat, error)
 	OnSubmitPostToPublisher(userUuid string, postUuid string) (string, error)
 	OnFetchOwnerPosts(userUuid string) ([]*entities.PostResDat, error)
 	OnDeletePostByUUID(postUuid string) error
